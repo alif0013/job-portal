742944fce9ad71b8f6e32eb7183c18df92789ebe
alif0013 <mdalifh36@gmail.com>
2024-03-27 01:45:11 +0600

    some design change

--- src/Components/LeftSideFilter.jsx
+++ src/Components/LeftSideFilter.jsx
@@ -6,73 +6,79 @@ const LeftSideFilter = () => {
         <div>
 
             {/* Location part */}
-            <h1 className='text-2xl pb-2 font-semibold'>Filters</h1>
-            <p className='font-semibold  mb-3'>Location</p>
+            <div>
+                <h1 className='text-2xl pb-2 font-semibold'>Filters</h1>
+                <p className='font-semibold  mb-3'>Location</p>
 
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <p className='text-black/60 font-semibold'>Near Me</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <p className='text-black/60 font-semibold '>Remote Job</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <p className='text-black/60 font-semibold '>WithIn 15 km</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <p className='text-black/60 font-semibold '>WithIn 30 km</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <p className='text-black/60 font-semibold '>WithIn 50 km</p>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <p className='text-black/60 font-semibold'>Near Me</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <p className='text-black/60 font-semibold '>Remote Job</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <p className='text-black/60 font-semibold '>WithIn 15 km</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <p className='text-black/60 font-semibold '>WithIn 30 km</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <p className='text-black/60 font-semibold '>WithIn 50 km</p>
+                </div>
             </div>
 
             {/* Salary Part */}
-            <h1 className='text-2xl pb-2 mt-2 font-semibold'>Salary</h1>
+            <div>
+                <h1 className='text-2xl pb-2 mt-2 font-semibold'>Salary</h1>
 
-            <div className='flex gap-2 mt-2 mb-3'>
-                <button className='border-2 border-gray-200 px-2 py-1 hover:bg-gray-200 hover:border-blue hover:text-blue hover:drop-shadow-md rounded-sm'>Hourly</button>
-                <button className='border-2 border-gray-200 px-2 py-1 hover:bg-gray-200 hover:border-blue hover:text-blue hover:drop-shadow-md rounded-sm'>Monthly</button>
-                <button className='border-2 border-gray-200 px-2 py-1 hover:bg-gray-200 hover:border-blue hover:text-blue hover:drop-shadow-md rounded-sm'>Yearly</button>
+                <div className='flex gap-2 mt-2 mb-3'>
+                    <button className='border-2 border-gray-200 px-2 py-1 hover:bg-gray-200 hover:border-blue hover:text-blue hover:drop-shadow-md rounded-sm'>Hourly</button>
+                    <button className='border-2 border-gray-200 px-2 py-1 hover:bg-gray-200 hover:border-blue hover:text-blue hover:drop-shadow-md rounded-sm'>Monthly</button>
+                    <button className='border-2 border-gray-200 px-2 py-1 hover:bg-gray-200 hover:border-blue hover:text-blue hover:drop-shadow-md rounded-sm'>Yearly</button>
 
+                </div>
+                {/* salary Inpuut */}
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <p className='text-black/60 font-semibold'>Any</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
+                    <p className='text-black/60 font-semibold'> 30,000 K</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
+
+                    <p className='text-black/60 font-semibold'> 40,000 K</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
+
+                    <p className='text-black/60 font-semibold'> 50,000 K</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
+
+                    <p className='text-black/60 font-semibold'> 60,000 K</p>
+                </div>
+                <div className='flex gap-2 items-center pb-1'>
+                    <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
+                    <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
+
+                    <p className='text-black/60 font-semibold'> 80,000 K</p>
+                </div>
             </div>
-            {/* salary Inpuut */}
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <p className='text-black/60 font-semibold'>Any</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>   
-                <p className='text-black/60 font-semibold'> 30,000 K</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
-              
-                <p className='text-black/60 font-semibold'> 40,000 K</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
-              
-                <p className='text-black/60 font-semibold'> 50,000 K</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
-              
-                <p className='text-black/60 font-semibold'> 60,000 K</p>
-            </div>
-            <div className='flex gap-2 items-center pb-1'>
-                <span className='pt-1'><input type="radio" name="radio-1" className="radio h-[15px] w-[15px]" /></span>
-                <span className='text-black/80 font-bold'><LiaAngleRightSolid></LiaAngleRightSolid> </span>
-              
-                <p className='text-black/60 font-semibold'> 80,000 K</p>
-            </div>
+
+            
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -25,14 +25,14 @@ const Home = () => {
             {/* job post card section */}
 
             <div className='bg-[#FAFAFA]'>
-                <div className='grid grid-cols-1 px-5 md:p-10  md:grid-cols-12 md:max-w-[1200px] md:mx-auto gap-5'>
-                    <div className='bg-white rounded-md md:col-span-3 p-5 drop-shadow-md'>
+                <div className='grid grid-cols-1 md:grid-cols-1 md:p-10  lg:grid-cols-12 lg:max-w-[1200px] md:mx-auto gap-5'>
+                    <div className='bg-white rounded-md lg:col-span-3 p-5 drop-shadow-md'>
                         <LeftSideFilter></LeftSideFilter>
                     </div>
-                    <div className='bg-white rounded-md md:col-span-6 p-5  border-2 border-yellow-600'>
+                    <div className='bg-white rounded-md lg:col-span-6 p-5  border-2 border-yellow-600'>
                         <MiddleSideCard></MiddleSideCard>
                     </div>
-                    <div className='bg-white rounded-md md:col-span-3 p-5 border-2 border-green-400'>
+                    <div className='bg-white rounded-md lg:col-span-3 p-5 border-2 border-green-400'>
                         <RightSideNotify></RightSideNotify>
                     </div>
 
