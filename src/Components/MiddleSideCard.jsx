21f82d14171606b3b6158c1b510bf540e469d329
alif0013 <mdalifh36@gmail.com>
2024-03-29 00:27:04 +0600

    added filter design

--- src/Components/MiddleSideCard.jsx
+++ src/Components/MiddleSideCard.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const MiddleSideCard = () => {
     return (
         <div>
-            middle side card
+            {/* filter and all job length */}
+            <div className='flex justify-between items-center'>
+                <h1>3128 JObs</h1>
+                <div>
+                    <select className="select select-bordered select-sm w-full max-w-xs">
+                        <option disabled selected>Small</option>
+                        <option>Small Apple</option>
+                        <option>Small Orange</option>
+                        <option>Small Tomato</option>
+                    </select>
+                </div>
+            </div>
         </div>
     );
 };
