bed43aaaa591ae30157aecc1af44c448d601eccc
alif0013 <mdalifh36@gmail.com>
2024-03-30 00:38:17 +0600

    remove alljob card compo

--- src/Components/MiddleSideCard.jsx
+++ src/Components/MiddleSideCard.jsx
@@ -1,13 +1,25 @@
 import { IoFilter } from "react-icons/io5";
-import AllJob from "./AllJobCard/AllJob";
+import { useEffect, useState } from "react";
+import JobCard from "./AllJobCard/JobCard";
 
 const MiddleSideCard = () => {
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch("jobs.json")
+            .then(res => res.json())
+            .then(data => {
+                setJobs(data);
+            })
+    }, [])
+
     return (
         <div>
             {/* filter and all job length */}
             <div className='flex justify-between items-center'>
                 <h1 className="text-2xl md:text-3xl font-semibold">3128 Jobs</h1>
-                
+
                 <div className="flex items-center gap-2 px-2 py-1 border-gray-200 border-2 drop-shadow-lg">
                     <IoFilter className=""></IoFilter> <h2>Filter By</h2>
                     <select className="border-none outline-none" name="example" id="example">
@@ -22,7 +34,13 @@ const MiddleSideCard = () => {
             </div>
 
             {/* all jobs card  */}
-            <AllJob></AllJob>
+            <div>
+                {
+                    jobs.map(job => <JobCard job={job} key={job.id}></JobCard>)
+                }
+
+            </div>
+
 
         </div>
     );
