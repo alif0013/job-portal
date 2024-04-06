929dffb2a3f2f3cb96a327d9569eb1a1e701fc8e
alif0013 <mdalifh36@gmail.com>
2024-04-06 23:01:30 +0600

    changes some design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
                             </svg>
                         </button>
-                    </div>
+                    </div> 
                 </div>
             </div>
             {/* Mobile menu */}
